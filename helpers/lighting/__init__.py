4bb1b0939bc32838d59ade5c95c06fa0574f6cab
gyulababa <gyulababa@gmail.com>
2026-02-10 09:26:47 +0100

    feat(guardrail): expose lighting public API

---
+++ helpers/lighting/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from .pixel_buffer_editor import PixelBufferEditor
+from .pixel_strips_model import (
+    Endpoint,
+    PixelColorRGB,
+    StripType,
+    apply_master_brightness_to_rgb_triplet,
+    normalize_master_brightness,
+    seed_pixel_strips_doc,
+    seed_strip_raw,
+)
+
+__all__ = [
+    "Endpoint",
+    "PixelBufferEditor",
+    "PixelColorRGB",
+    "StripType",
+    "apply_master_brightness_to_rgb_triplet",
+    "normalize_master_brightness",
+    "seed_pixel_strips_doc",
+    "seed_strip_raw",
+]
